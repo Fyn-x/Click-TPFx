7d7fd524647834be2d8d6f9dc22a51df3428e58a
IT-TPFX\tpfx_ <fajri@tpfx.co.id>
2024-04-18 15:54:56 +0700

    Update: added api configuration

--- config/api.php
+++ config/api.php
@@ -1,9 +1,10 @@
 <?php
 
 return [
-
     'api_client_id' => env('MT4_BASE_URL'),
     'getdepwith' => 'apigetdepwith.php',
     'gettrades' => 'apigettrades.php',
-    'getequitybulk' => 'apiequitybulk.php'
+    'getequitybulk' => 'apiequitybulk.php',
+    'api_url_leads' => env('API_URL_LEADS'),
+    'api_crm_leads_token' => env('API_CRM_LEADS_TOKEN')
 ];
